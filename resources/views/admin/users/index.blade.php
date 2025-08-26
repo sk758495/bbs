11e929f1bd35df04824a62b5dac30ba16868c80f
sk758495 <sk758495@gmail.com>
2025-08-26 15:21:26 +0530

    nkwnfkcjed

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -14,6 +14,13 @@
             </div>
         @endif
 
+        @if(session('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('error') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            </div>
+        @endif
+
         <div class="card border-0 shadow-sm">
             <div class="card-body">
                 <div class="table-responsive">
